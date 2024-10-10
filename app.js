fa441b772b421df9b93568bb1514f47a00b596cf
Lavat Moderador <lavatdados@gmail.com>
2024-10-10 13:20:11 -0300

    Primeira tentativa site

---
+++ app.js
@@ -0,0 +1,9 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    document.getElementById("message").textContent = "Você clicou no botão!";
+});
+
+
+function openAgenda() {
+    window.location.href = "agenda.html"; // Substitua pelo link correto da página de agenda
+  }
+  
