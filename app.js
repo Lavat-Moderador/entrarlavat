6a3d4bb8e4e30159eeb982fcf79077dcc7efa058
Lavat Moderador <lavatdados@gmail.com>
2024-10-22 13:23:01 -0300

    Aba de documentos com manual funcionando
    O manual pode ser visualizado antes de ter que baixar

--- app.js
+++ app.js
@@ -7,16 +7,3 @@ function openAgenda() {
     window.location.href = "agenda.html"; // Substitua pelo link correto da página de agenda
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('reveal-button').addEventListener('click', function() {
-        var maisSobre = document.getElementById('mais-sobre');
-        var seta = document.querySelector('.seta');
-        if (maisSobre.style.display === 'none' || maisSobre.style.display === '') {
-            maisSobre.style.display = 'block';
-            seta.style.transform = 'rotate(180deg)'; // Gira a seta para cima
-        } else {
-            maisSobre.style.display = 'none';
-            seta.style.transform = 'rotate(0deg)'; // Gira a seta para baixo
-        }
-    });
-});
