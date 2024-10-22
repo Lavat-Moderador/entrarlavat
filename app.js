9a0fc89799c5ca63129d51e7c0c2af8151aba1a9
Lavat Moderador <lavatdados@gmail.com>
2024-10-22 11:02:26 -0300

    Scroll suavizado quando clica

--- app.js
+++ app.js
@@ -5,54 +5,18 @@ document.getElementById("myButton").addEventListener("click", function() {
 
 function openAgenda() {
     window.location.href = "agenda.html"; // Substitua pelo link correto da página de agenda
-  }
-  
+}
 
-  const menuItems = document.querySelectorAll('.menu a[href^="#0"]');
-
-  function getScrollTopByHref(element) {
-    const id = element.getAttribute('href');
-    return document.querySelector(id).offsetTop;
-  }
-  //criando a função de rolar para uma região
-  function scrollToPosition(to) {
-    smoothScrollTo(0, to);
-  }
-  
-  function scrollToIdOnClick(event) {
-    event.preventDefault();
-    const to = getScrollTopByHref(event.currentTarget) - 80;
-    scrollToPosition(to);
-  }
-  
-  menuItems.forEach(item => {
-    item.addEventListener('click', scrollToIdOnClick);
-  });
-  
-  //suporte a browsers antigos / que não suportam scroll smooth nativo por meio do script:
-  
-  function smoothScrollTo(endX, endY, duration) {
-    const startX = window.scrollX || window.pageXOffset;
-    const startY = window.scrollY || window.pageYOffset;
-    const distanceX = endX - startX;
-    const distanceY = endY - startY;
-    const startTime = new Date().getTime();
-  
-    duration = typeof duration !== 'undefined' ? duration : 400;
-  
-    // Easing function
-    const easeInOutQuart = (time, from, distance, duration) => {
-      if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
-      return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
-    };
-  
-    const timer = setInterval(() => {
-      const time = new Date().getTime() - startTime;
-      const newX = easeInOutQuart(time, startX, distanceX, duration);
-      const newY = easeInOutQuart(time, startY, distanceY, duration);
-      if (time >= duration) {
-        clearInterval(timer);
-      }
-      window.scroll(newX, newY);
-    }, 1000 / 60); // 60 fps
-  };
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('reveal-button').addEventListener('click', function() {
+        var maisSobre = document.getElementById('mais-sobre');
+        var seta = document.querySelector('.seta');
+        if (maisSobre.style.display === 'none' || maisSobre.style.display === '') {
+            maisSobre.style.display = 'block';
+            seta.style.transform = 'rotate(180deg)'; // Gira a seta para cima
+        } else {
+            maisSobre.style.display = 'none';
+            seta.style.transform = 'rotate(0deg)'; // Gira a seta para baixo
+        }
+    });
+});
